3275844ece8f02a2818c9d47e45542b0cc15ac43
Muhammad Syihabuddin Az Zuhair <15320135+msazzuhair@users.noreply.github.com>
2020-07-16 16:52:28 +0700

    Update migration version

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = true;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 0;
+$config['migration_version'] = 1;
 
 /*
 |--------------------------------------------------------------------------
